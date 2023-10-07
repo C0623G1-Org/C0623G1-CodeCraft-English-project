f0e83ca9340e9b980c768065333cef6267323071
duydd-c0623g1 <daoducduy.work@gmail.com>
2023-10-07 09:21:16 +0700

    update css

--- src/main/java/com/example/english/controller/UserServlet.java
+++ src/main/java/com/example/english/controller/UserServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
 
 @WebServlet(name = "UserServlet", value = "")
 public class UserServlet extends HttpServlet {
@@ -32,50 +31,10 @@ public class UserServlet extends HttpServlet {
             case "logOut":
                 logOut(request, response);
                 break;
-
-            case "delete":
-                deleteUser(request,response);
-                break;
-            case "selectAll":
-                showListUser(request,response);
-                break;
-            case "fill-form":
-                fillForm(request,response);
-                break;
             default:
                 homePage(request, response);
                 break;
         }
-    }
-
-    private void fillForm(HttpServletRequest request, HttpServletResponse response) {
-        User user = userService.getByIdUser(Integer.parseInt(request.getParameter("id")));
-            request.setAttribute("user",user);
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/edit_my_page.jsp");
-        try {
-            requestDispatcher.forward(request,response);
-        } catch (ServletException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void showListUser(HttpServletRequest request, HttpServletResponse response) {
-        List<User> userList =userService.selectAllUser();
-        request.setAttribute("users",userList);
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/admin.jsp");
-        try {
-            requestDispatcher.forward(request,response);
-        } catch (ServletException | IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void deleteUser(HttpServletRequest request, HttpServletResponse response){
-        int id = Integer.parseInt(request.getParameter("id"));
-        userService.deleteUser(id);
-        showListUser(request,response);
     }
 
     private void logOut(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -88,23 +47,21 @@ public class UserServlet extends HttpServlet {
         int id = Integer.parseInt(request.getParameter("id"));
         try {
             User getIdUser = userService.getByIdUser(id);
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/my_page.jsp");
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/edit_my_page.jsp");
             request.setAttribute("getIdUser",getIdUser);
             requestDispatcher.forward(request,response);
         } catch (ServletException | IOException e) {
             e.printStackTrace();
         }
     }
+
     private void editUserById(HttpServletRequest request,HttpServletResponse response){
-        int id = Integer.parseInt(request.getParameter("userId"));
-        String name = request.getParameter("userName");
-        String email = request.getParameter("email");
-        String dob = request.getParameter("dob");
-        User user = new User(id,name,email,dob);
-        userService.editUser(user);
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/my_page.jsp");
-        request.setAttribute("user",user);
+        int id = Integer.parseInt(request.getParameter("id"));
+        RequestDispatcher requestDispatcher = null;
         try {
+            User user = userService.getByIdUser(id);
+            request.setAttribute("user",user);
+            requestDispatcher = request.getRequestDispatcher("/edit_my_page.jsp");
             requestDispatcher.forward(request,response);
         } catch (ServletException e) {
             e.printStackTrace();
@@ -159,12 +116,12 @@ public class UserServlet extends HttpServlet {
         if (user != null) {
             HttpSession session = request.getSession();
             session.setAttribute("user", user);
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/homePage.jsp");
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("homePage.jsp");
             requestDispatcher.forward(request, response);
         } else {
             String error  = "Sai tên đăng nhập hoặc mật khẩu";
-            request.setAttribute("error", error);
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/login.jsp");
+            request.setAttribute("loginError", error);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("homePage.jsp");
             requestDispatcher.forward(request, response);
         }
     }
@@ -177,11 +134,11 @@ public class UserServlet extends HttpServlet {
         String error = "Xác nhận mật khẩu không chính xác";
         String emailExist = "Địa chỉ email đã tồn tại";
         if (password.equals(confirm)) {
-            User user = new User(email, name,  password);
+            User user = new User(email, name, password);
             if (userService.signup(user)) {
                 request.setAttribute("name", name);
                 request.setAttribute("password", password);
-                RequestDispatcher requestDispatcher = request.getRequestDispatcher("/login.jsp");
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("homePage.jsp");
                 requestDispatcher.forward(request, response);
             } else {
                 request.setAttribute("error", emailExist);
@@ -196,7 +153,7 @@ public class UserServlet extends HttpServlet {
     }
 
     private void homePage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/homePage.jsp");
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("homePage.jsp");
         requestDispatcher.forward(request, response);
     }
 }
