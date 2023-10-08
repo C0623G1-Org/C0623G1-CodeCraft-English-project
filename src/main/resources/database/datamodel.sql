e162fc65f10ebfccd5afa7018b091a397cd9e95f
LyDoan0302 <138420217+LyDoan0302@users.noreply.github.com>
2023-10-08 14:39:00 +0700

    backup sql

--- src/main/resources/database/datamodel.sql
+++ src/main/resources/database/datamodel.sql
@@ -129,6 +129,154 @@ VALUES (1,'Who are all ________ people?','this','those','them','that','those'),
        (3, "This library card will give you free access____the Internet eight hours a day","on","to","from","in","to"),
        (3, "She had to hand in her notice____advance when she decided to leave the job","with","from","in","to","in"),
        (3, "Young people have become increasingly commited____social activities","of","to","in","at","to");
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "The sun rises in the _________.", "east", "west", "north", "south", "east");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "She was feeling very tired, ________ she went to bed early.", "so", "but", "and", "because", "so");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The __________ of this novel is its intricate plot.", "author", "cover", "theme", "protagonist", "theme");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "I enjoy ________ books in my free time.", "read", "reading", "reads", "to read", "reading");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "The Eiffel Tower is located in _________.", "Paris", "Rome", "London", "Berlin", "Paris");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The scientist made a groundbreaking discovery, ________ will change the course of medicine.", "it", "which", "who", "that", "which");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "She didn't have much time, ________ she worked quickly.", "but", "so", "because", "yet", "but");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The ___________ of the company was evident in its strong financial performance.", "CEO", "leadership", "vision", "employees", "leadership");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "I need to buy some ___________ for my school project.", "books", "paper", "computers", "teachers", "paper");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "I have two _____ brothers.", "older", "eldest", "more older", "most old", "older");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "He is the _____ I know.", "funnier", "funniest", "funny", "fun", "funniest");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The artist painted the mural with great ________.", "care", "art", "paint", "canvas", "care");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "Can you please pass me the _____?", "salt", "salts", "salty", "salted", "salt");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "The car is parked ________ the garage.", "in", "on", "at", "with", "in");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The company's success is attributed to its innovative ________.", "product", "products", "product's", "products'", "products");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "I want to go to the park _____ play soccer.", "or", "and", "but", "so", "and");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "I don't have _____ money to buy a new phone.", "any", "some", "many", "much", "any");
+
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The jury delivered a ________ verdict.", "guilt", "guilty", "guiltier", "guiltiest", "guilty");
+
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "My cat loves to chase ________.", "mouses", "mices", "mice", "mouses'", "mice");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "We went to the store _____ buy groceries.", "for", "at", "on", "with", "for");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The athlete set a new ________ record in the long jump.", "world", "worlds", "worldly", "world's", "world");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "My grandmother bakes delicious ________.", "cookie", "cookies", "cookie's", "cookies'", "cookies");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "The movie was so _____ that I fell asleep.", "boring", "bored", "bore", "bores", "boring");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The novel's plot was filled with unexpected ________.", "twist", "twists", "twisted", "twisting", "twists");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "My sister has a _____ dog named Max.", "black", "blacks", "black's", "blacks'", "black");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "He always eats his lunch _____.", "alone", "lonely", "lonesome", "lonely's", "alone");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The CEO's decision had a profound ________ on the company.", "affect", "effect", "affected", "effecting", "effect");
+
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The artist used vibrant ________ in the painting.", "colors", "hues", "shades", "tints", "colors");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "I need to buy some ________ for my garden.", "flowers", "trees", "vegetables", "rocks", "flowers");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "My mom is a great ________.", "chef", "painter", "musician", "athlete", "chef");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "The concert was so ________ that it sold out in minutes.", "popular", "crowded", "entertaining", "loud", "popular");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The ________ of the orchestra conducted the performance.", "director", "conductor", "composer", "soloist", "conductor");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "My dog likes to play fetch with a ________.", "ball", "stick", "rope", "frisbee", "ball");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "She was so ________ that she couldn't stop laughing.", "amused", "amusing", "amuses", "amusement", "amused");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (3, "The ________ of the novel left readers in suspense.", "climax", "plot", "theme", "character", "climax");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (1, "I need to buy a dozen ________ for the party.", "cupcakes", "cookies", "cakes", "muffins", "cupcakes");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES (2, "The sun ________ in the east.", "rises", "sets", "shines", "hides", "rises");
+
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES
+    (1, "My dog likes to play fetch with a ________.", "ball", "stick", "rope", "frisbee", "ball"),
+    (1, "I saw a beautiful ________ in the garden.", "flower", "tree", "rock", "car", "flower"),
+    (1, "The ________ in the sky is shining brightly.", "sun", "moon", "star", "cloud", "sun"),
+    (1, "Can you pass me the salt and ________?", "pepper", "sugar", "coffee", "tea", "pepper"),
+    (1, "The baby is sleeping in the ________.", "crib", "bathtub", "refrigerator", "oven", "crib"),
+    (1, "I like to eat pizza with ________.", "cheese", "ice cream", "ketchup", "strawberries", "cheese"),
+    (1, "My favorite color is ________.", "blue", "chair", "book", "table", "blue");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES
+    (2, "He ________ his keys on the kitchen counter.", "left", "leaves", "leaving", "lefted", "left"),
+    (2, "She plays the piano ________ than her brother.", "beautifully", "more beautifully", "beautiful", "beautify", "more beautifully"),
+    (2, "He is very ________ about his work.", "passionate", "boring", "sleepy", "confused", "passionate"),
+    (2, "The ________ of the movie made me cry.", "ending", "beginning", "middle", "credits", "ending"),
+    (2, "She always tells funny ________.", "jokes", "songs", "stories", "dances", "jokes"),
+    (2, "He likes to travel to ________ countries.", "exotic", "simple", "boring", "quiet", "exotic"),
+    (2, "I can't find my keys anywhere. They must be ________.", "lost", "found", "hidden", "discovered", "lost"),
+    (2, "The movie was so ________ that I watched it twice.", "exciting", "boring", "long", "short", "exciting"),
+    (2, "She ________ her dog every morning.", "walks", "walked", "walking", "walker", "walks");
+
+INSERT INTO questions (diff_id, question_content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES
+    (3, "The ________ of the company is to provide excellent customer service.", "objective", "purpose", "mission", "vision", "objective"),
+    (3, "The ________ in the sky was mesmerizing.", "constellation", "moon", "comet", "asteroid", "constellation"),
+    (3, "The ________ sculpture in the museum was a masterpiece.", "contemporary", "ancient", "modern", "historic", "contemporary"),
+    (3, "The ________ of the play was thought-provoking.", "dialogue", "monologue", "script", "screenplay", "dialogue"),
+    (3, "The ________ of the company are known for their innovation.", "founders", "employees", "customers", "competitors", "founders"),
+    (3, "The detective solved the ________ case.", "mysterious", "simple", "complicated", "easy", "mysterious"),
+    (3, "The chef prepared a ________ dish for the special occasion.", "gourmet", "fast-food", "homemade", "spicy", "gourmet"),
+    (3, "The ________ of the museum guided us through the art collection.", "curator", "visitor", "artist", "critic", "curator");
+
 
 DELIMITER $$
 CREATE PROCEDURE create_practice_list_by_id(diff_id int)
